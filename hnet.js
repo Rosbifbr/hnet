075bfef07e20d9a07965caa8ff2992d7769162cd
Rodrigo Ourique <rodrigo.guima.ourique@gmail.com>
2023-04-29 16:38:44 -0300

    Neurons are now copyable. Still horrible code. Init function works as it should but feed forward does not

--- hnet.js
+++ hnet.js
@@ -1,7 +1,7 @@
-//Application-specific functions
+//Application-specific functions. I'll leave some defaults here for the problems that I want to solve.
 const sigmoid = x => 1 / (1 + Math.exp(-x))
 const sigmoid_derivative = x => sigmoid(x) * (1 - sigmoid(x))
-const cross_entropy_loss_function = (y_true, y_pred) => {
+const cross_entropy_loss = (y_true, y_pred) => {
         let loss = 0
         for (let i in y_true) {
                 loss += y_true[i] * Math.log(y_pred[i]) + (1 - y_true[i]) * Math.log(1 - y_pred[i])
@@ -9,19 +9,27 @@ const cross_entropy_loss_function = (y_true, y_pred) => {
         return -loss
 }
 
-//Data-Structs
-const neuron = {
-        weights: [],
+//Globals
+const defaults = {
         activation_function: sigmoid,
-	activation_derivative: sigmoid_derivative,
-        value:0,
+        activation_derivative: sigmoid,
+        cost_function: cross_entropy_loss,
 }
 
-exports.base_topology = [
-        [neuron.structuredClone(), neuron.structuredClone(),], //This is an input layer, not a hidden one.
-        [neuron.structuredClone(), neuron.structuredClone(), neuron.structuredClone()],
-        [neuron.structuredClone(), neuron.structuredClone(), neuron.structuredClone()],
-        [neuron.structuredClone()], //Output 
+//Neuron class. Maybe will completely ditch classical OOP later. 
+class Neuron {
+        constructor() {
+                this.weights = []
+                this.value = 0
+        }
+}
+
+//FIXME: This has no reason to be initialized before the init function. Rewrite the function to accept topology "maps" and return the actual network.
+exports.example_topology = [
+        [new Neuron()], //Input.
+        [new Neuron(), new Neuron(), new Neuron()],
+        [new Neuron(), new Neuron(), new Neuron()],
+        [new Neuron()], //Output 
 ]
 
 //We need to initialize input values on each neuron and randomize the weights. "Wire" the neurons together.
@@ -34,26 +42,25 @@ exports.init = (topology) => {
                         let neuron_obj = topology[layer][neuron]
                         for (index in topology[layer-1]) {
                                 neuron_obj.weights.push(Math.random())
-                               // neuron_obj.weights.push(1) //TODO: REMOVE
                         }
                 }
         }
         return topology
 }
 
+//Topology WILL be modified by being passed in here.
 exports.feed_forward = (topology, inputs) => {
-        let tmp_topology = topology.structuredClone() //FIXME: This copy is ugly and won't scale.
-        for (i in tmp_topology[0]) tmp_topology[0][i].value = inputs[i]
-        for (layer in tmp_topology) {
+        for (i in topology[0]) topology[0][i].value = inputs[i]
+        for (layer in topology) {
                 if (layer == 0) continue //We won't calculate the weight of the inputs.
-                for (let neuron of tmp_topology[layer]){
-                        neuron.value = neuron.activation_function(neuron.weights.reduce((total, weight_value, index) => {
-                                let behind_neuron_value = tmp_topology[layer-1][index].value
+                for (let neuron of topology[layer]){
+                        neuron.value = defaults.activation_function(neuron.weights.reduce((total, weight_value, index) => {
+                                let behind_neuron_value = topology[layer-1][index].value
                                 total += weight_value * behind_neuron_value
                         }))
                 }
         }
-        return tmp_topology.slice(-1).map(n => n.value) //Returns the value of the output neurons.
+        return topology.slice(-1).map(n => n.value) //Returns the value of the output neurons.
 }
 
 //TODO:
