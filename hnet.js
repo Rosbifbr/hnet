e26ec33ea6c0505ca955a146dc6525c15ae0019a
Rodrigo Ourique <rodrigo.guima.ourique@gmail.com>
2023-08-05 13:17:48 -0300

    Added bias value, cleaned up code

--- hnet.js
+++ hnet.js
@@ -1,4 +1,5 @@
 //Application-specific functions. I'll leave some defaults here for the problems that I want to solve.
+const relu = x => x > 0 ? x : 0
 const sigmoid = x => 1 / (1 + Math.exp(-x))
 const sigmoid_derivative = x => sigmoid(x) * (1 - sigmoid(x))
 const cross_entropy_loss = (y_true, y_pred) => {
@@ -16,18 +17,17 @@ const defaults = {
         cost_function: cross_entropy_loss,
 }
 
-//TODO: Implement biases for the neurons. I was too lazy to have done it until now
-//Neuron class. Maybe will completely ditch classical OOP later. 
+//TODO: Implement biases for the neurons. I was too lazy to have done it until now 
 class Neuron {
         constructor() {
+                this.bias = 1; //0 to 1
                 this.weights = []
                 this.value = 0
         }
 }
 
-//FIXME: This has no reason to be initialized before the init function. Rewrite the function to accept topology "maps" and return the actual network.
+//Simple example with two hidden layers.
 exports.example_topology = [
-        [new Neuron()], //Input.
         [new Neuron(), new Neuron(), new Neuron()],
         [new Neuron(), new Neuron(), new Neuron()],
         [new Neuron()], //Output.
@@ -35,14 +35,14 @@ exports.example_topology = [
 
 //We need to initialize input values on each neuron and randomize the weights. "Wire" the neurons together.
 exports.init = (topology) => {
-        for (layer in topology){
-                if (layer == 0) continue //Won't map the weights of inputs As they dont have predecessors.
+        for (layer in topology) {
+                //Won't map the weights of inputs As they dont have predecessors.
+                if (layer == 0) continue
 
                 //Assigining random weights.
-                for (let neuron in topology[layer]){
-                        let neuron_obj = topology[layer][neuron]
-                        for (index in topology[layer-1]) {
-                                neuron_obj.weights.push(Math.random())
+                for (let neuron of topology[layer]) {
+                        for (let neuron_ant of topology[layer - 1]) {
+                                neuron.weights.push(Math.random())
                         }
                 }
         }
@@ -50,20 +50,26 @@ exports.init = (topology) => {
 }
 
 exports.feed_forward = (topology, inputs) => {
-        for (i in topology[0]) {
-                topology[0][i].value = inputs[i]
-        }
+        //Mapping inputs as neurons for simplicity of the other. This is lazy.
+        topology.unshift(inputs.map(e => {
+                let neuron = new Neuron()
+                neuron.value = e
+                return neuron
+        }))
 
         for (layer in topology) {
-                if (layer == 0) continue //Inputs have no weigths
-                for (let neuron of topology[layer]){
-                        neuron.value = defaults.activation_function(neuron.weights.reduce((total, value, i) => {
-                                let behind_neuron_value = topology[layer-1][i].value
-                                return total + (value * behind_neuron_value)
+                //Inputs have no weigths
+                if (layer == 0) continue
+                
+                //Add behind neuron values multiplied by their weight, then apply activation function. 
+                for (let neuron of topology[layer]) {
+                        neuron.value = defaults.activation_function(neuron.weights.reduce((total, weight, i) => {
+                                let behind_neuron_value = topology[layer - 1][i].value
+                                return total + (weight * behind_neuron_value)
                         }))
                 }
         }
-        return topology.slice(-1)[0].map(n => n.value) //Getting output neuron values.
+        return topology.slice(-1).map(n => n.value) //Getting output neuron values.
 }
 
 //TODO:
